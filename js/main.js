500f20fe8849eb755cc2cf38a4bc6571e4254f4e
Edward Wright <ed.wright@live.co.uk>
2019-03-31 16:34:47 +0100

    0.2 MVP

--- js/main.js
+++ js/main.js
@@ -51,24 +51,24 @@ acronyms = [
     }
 ];
 
-$.each(acronyms, function(i, field){
-    console.log(field);
-});
+// $.each(acronyms, function(i, field){
+//     console.log(field);
+// });
 
 // listen for keypress within the searchbar
 $(".searchTerm").keyup(function(){
     if($('.searchTerm').val()) {
-        console.log("I have a value");
+        // console.log("I have a value");
         gotSearchTerm();
     } else {
-        console.log("I no longer have a value");
+        // console.log("I no longer have a value");
         notGotSearchTerm();
     }
 });
 
 // check if the searchbar has a value at initial loading
 if($('.searchTerm').val()) {
-    console.log("I already have a value");
+    // console.log("I already have a value");
     gotSearchTerm();
 };
 
@@ -77,8 +77,8 @@ function gotSearchTerm() {
     $(".branding").hide();
     $(".search-container").addClass("search-container-top");
     $(".ac-container").show();
-
-}
+    performSearch();
+};
 
 function notGotSearchTerm() {
     $(".title").show();
@@ -86,3 +86,16 @@ function notGotSearchTerm() {
     $(".search-container").removeClass("search-container-top");
     $(".ac-container").hide();
 }
+
+function performSearch() {
+    search = $('.searchTerm').val()
+    output = "";
+    $.each(acronyms, function(i, field){
+        if(~field.acronym.toLowerCase().indexOf(search.toLowerCase())) {
+            output += "<tr>\n<td>" + field.acronym + "</td>\n<td>" + field.full + "</td>\n<td>" + field.description + "</td>\n</tr>\n";
+            console.log(output);
+        }
+    })
+    $(".ac-container table tbody").empty(output);
+    $(".ac-container table tbody").append(output);
+};
